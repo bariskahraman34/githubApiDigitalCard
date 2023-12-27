26d19be45b9764b3e458efb67e2b4859b1b7f4b3
Barış Kahraman <baris-kahra@hotmail.com>
2023-12-27 03:52:12 +0300

    Card işlemleri tamamlandı gelen data da veri yoksa no information yazılması için ternery operatörü kullanıldı. kullanıcının varlık kontrolleri yapıldı başlangıçta default olarak iki kişinin getirilmesi olayı tamamlandı. getirilen kullanıcının kaldırılma işlemi tamamlandı.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.card-container');
 const getPersonButton = document.querySelector('#getPerson');
 
 let savePerson = JSON.parse(localStorage.getItem('personEntries')) || [];
@@ -7,41 +7,102 @@ function savePersonToLocalStorage(){
     return localStorage.setItem('personEntries',JSON.stringify(savePerson));
 }
 
-getPersonButton.addEventListener('click',function(){
-    const answer = prompt('Hangi Kullanıcıyı Eklemek İstiyorsun ?');
-    fetchGithub(answer);
+function bindRemovePerson(){
+    const removeBtns = document.querySelectorAll('.remove-user');
+    for (const removeBtn of removeBtns) {
+        removeBtn.addEventListener('click',function(){
+            const answer = confirm('Bu kişiyi kaldırmak istediğinizden emin misiniz?');
+            if(answer){
+                let personCardIndex = parseInt(removeBtn.parentElement.parentElement.id)
+                savePerson.splice(personCardIndex,1);
+                savePersonToLocalStorage();
+                getPerson();
+            }
+        })
+    }
+}
+
+// let isAnyUserExist;
+getPersonButton.addEventListener('click',function(e){
+    e.preventDefault();
+    const answer = prompt('Lütfen eklemek istediğiniz kullanıcının, kullanıcı adını giriniz.');
+    // for (const localPerson of savePerson) {
+    //     if(answer == localPerson.login){
+    //         isAnyUserExist = 1
+    //     }
+    // }
+    // if(isAnyUserExist == 1){
+    //     alert('Bu kullanıcı zaten mevcut.');
+    // }else{
+    //     fetchGithub(answer)
+    // }
+    if(!answer){
+        return
+    }
+    if(exist(answer)){
+        alert('Bu kullanıcı zaten mevcut')
+    }else{
+        fetchGithub(answer);
+    }
 })
 
-async function fetchGithub(person){
-    const response = await fetch(`https://api.github.com/users/${person}`,{
-        headers:{Authorization:'Bearer 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'}
-    });
-    const data = await response.json();
-    savePerson.push({
-        avatar_url:data.avatar_url,
-        name:data.name,
-        company:data.company,
-        login:data.login,
-        email:data.email,
-        followers:data.followers,
-        following:data.following,
-        location:data.location,
-        blog:data.blog,
-        bio:data.bio,
-        html_url:data.html_url
+
+
+function exist(userName){
+    const filtered = savePerson.filter((person) => {
+        return person.login == userName;
     })
-    savePersonToLocalStorage();
-    getPerson();
+    return filtered.length > 0 ;
+}
+
+
+
+async function fetchGithub(person){
+    if(exist(person)){
+        return
+    }
+    
+    const response = await fetch(`https://api.github.com/users/${person}`);
+    
+    const data = await response.json();
+    console.log(data)
+    
+    if(response.ok == false){
+        alert('Böyle bir kullanıcı bulunmuyor.');
+    }else{
+        savePerson.push({
+            avatar_url:data.avatar_url,
+            name:data.name,
+            company:data.company,
+            login:data.login,
+            email:data.email,
+            followers:data.followers,
+            following:data.following,
+            location:data.location,
+            blog:data.blog,
+            bio:data.bio,
+            html_url:data.html_url,
+            twitter_username:data.twitter_username,
+            public_repos:data.public_repos
+        })
+        savePersonToLocalStorage();
+        getPerson();
+    }
+    
 }
 
 async function getPerson(){
     container.innerHTML = "";
+    let idCounter = 0;
     for (const person of savePerson) {
         container.innerHTML += 
             `
-            <div class="person-card">
+            <div class="person-card" id="${idCounter}">
                 <div class="background-image">
                     <img class="bg-img" src="assets/img/bg-image.png" alt="">
+                    <div class="remove-user">
+                        <i class="fa-solid fa-xmark remove-mark"></i>
+                    </div>
                 </div>
                 <span class="profile-image">
                     <img src="${person.avatar_url}" alt="">
@@ -79,18 +140,31 @@ async function getPerson(){
                         <i class="fa-solid fa-link"></i>
                         ${person.blog ? `<a href="${person.blog}" target="_blank">${person.blog}</a>` : "No blog information"}
                     </span>
+                    <span class="site">
+                        <i class="fa-brands fa-git-alt" style="margin-right:5px;" ></i>
+                        Public Repos:
+                        ${person.public_repos}
+                    </span>
                     <span class="bio">${person.bio ? person.bio : "No bio information"}</span>
                     <span class="social-media-links"> 
                         <a class="github-url" href="${person.html_url ? person.html_url : "No github information"}"target="_blank">
                             <i class="fa-brands fa-github fa-2x"></i>
                         </a>
+                        ${person.twitter_username ? `<a href="https://twitter.com/${person.twitter_username}" target="_blank">
+                        <i class="fa-brands fa-twitter fa-2x"></i>
+                        </a>`:""}
                     </span>
                 </div>
             </div>
             `
+            idCounter++;
     }
-    
+
+    bindRemovePerson();
 } 
 
+
 fetchGithub('bariskahraman34');
-fetchGithub('furkanczay');
+fetchGithub('furkanczay')
+
+getPerson();
